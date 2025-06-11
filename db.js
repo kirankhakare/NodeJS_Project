09fbd8d8ceb52a26cce68a6c14d9c6349b9aa891
kirankhakare <kirankhakare111@gmail.com>
2025-06-11 15:20:23 +0530

    second

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Kiran@123',
-  database: 'student_admissions'
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
 });
 connection.connect(err => {
   if (err){
